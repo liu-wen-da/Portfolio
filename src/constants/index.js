1b5c8c029131d22dbda5f9b5e0cf0c5aacd0d196
Wenda Liu <63420806+liu-wen-da@users.noreply.github.com>
2023-11-05 22:19:49 -0800

    got the experience section done

--- src/constants/index.js
+++ src/constants/index.js
@@ -15,14 +15,12 @@ import {
     git,
     figma,
     docker,
-    meta,
-    starbucks,
-    tesla,
-    shopify,
     carrent,
     jobit,
     tripguide,
     threejs,
+    lightcast,
+    scale,
   } from "../assets";
   
   export const navLinks = [
@@ -54,7 +52,7 @@ import {
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Software Developer",
       icon: creator,
     },
   ];
@@ -116,55 +114,29 @@ import {
   
   const experiences = [
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
-      icon: starbucks,
+      title: "Software Engineer",
+      company_name: "SCALE",
+      icon: scale,
+      iconBg: "#353A40",
+      date: "Aug 2020 - Dec 2021",
+      points: [
+        "Designed and developed a MVP mobile version of the company website with 100+ active users.",
+        "Added features for students to view, manage, and create accadmic schedule. Also incorporated a API to build a course recommendation system for the student base on their major and interest.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality product.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Data Analytics Intern",
+      company_name: "LightCast",
+      icon: lightcast,
       iconBg: "#383E56",
-      date: "March 2020 - April 2021",
+      date: "Sep 2022 - Jan 2024",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "React Native Developer",
-      company_name: "Tesla",
-      icon: tesla,
-      iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
-      iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Full stack Developer",
-      company_name: "Meta",
-      icon: meta,
-      iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Developing and maintaining applications using Python.",
+        "Developed new tools and tests to more efficiently process University data.",
+        "Implementing new features and optimizing existing scripts to improve performance and reliability.",
+        "Collaborating with the product team to design and implement new features to better automate the data processing pipeline.",
       ],
     },
   ];
